5043ea6ed6e040ebf60952a3c86af22b40af6dc4
Jean-Michel Michno <jmichno@helix15.cs.umn.edu>
2016-03-24 12:17:57 -0500

    Easy way to combine multiple sequences from different lanes of NG

---
+++ Catseq.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+#This script with take the base name of the files your are interested in
+#, the output directory, and the sequencing adapter
+#and combine all the forward and reverse reads into two files
+
+
+set -euo pipefail
+
+#sample base file name
+FILE="$1"
+#output directory
+OUT="$2"
+#Adapter seq
+ADAPT="$3"
+
+#combine all files with your base name, anything, R1, anything, .fastq
+cat ${FILE}*[R][1]*fastq \
+    > "${OUT}${FILE}_${ADAPT}_R1.fastq"
+
+
+cat ${FILE}*[R][2]*fastq \
+    > "${OUT}${FILE}_${ADAPT}_R2.fastq"
