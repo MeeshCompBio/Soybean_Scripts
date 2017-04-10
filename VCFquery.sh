d0f0ae17784598739707762ab2e0171ac4e1672d
Jean-Michel "Meesh <mich0391@umn.edu>
2017-04-09 20:12:53 -0500

    Added new tool for Stupar Lab
    Allow lab members to get indel/variant call for specific regions of the genome

---
+++ VCFquery.sh
@@ -0,0 +1,95 @@
+#ted by Jean-Michel Michno (Meesh)
+#email: mich039@umn.edu
+# module load cutadapt
+set -euo pipefail
+
+#use getopts for command line arguments
+while getopts hc:s:t:f::o: flag; do
+    case $flag in
+      #this is the help command
+        h)
+            echo "This is your help information:
+            This script was designed for use on MSI by the Stupar Lab.
+            The VCF file paths are hardcoded to a public directory
+            Usage:
+            VCFquery.sh -c Chromosome -s start_position -t stop position  -o output_prefix
+            Ex:
+            VCFquery.sh -c 01 -s 1234 -t 5678  -o myfile
+            
+            Options:
+                    -c Chrmosome to use <only need a TWO digit number 01-20>
+                    -s start position (number)
+                    -t True :If flag is used, then bowtie will be used (default bwa)
+                    -f which lines to searc, use wither 106 or NAM (106 is default)
+                    -t top position (number)
+                    -o output file prefix, what you want to file to be called 
+                        without an extension
+                    "
+            exit 2
+            ;;
+        c)
+            echo " Looking for: Chr$OPTARG";
+            CHROM=$OPTARG
+            FILE="/mnt/d/NAMVCF/NAM_Parent_Playpus.vcf"
+            HEADER="/mnt/d/NAMVCF/PlatHeaders.vcf"
+            ;;
+        s)
+            echo "Start postion is: $OPTARG";
+            START=$OPTARG
+            ;;
+        t)
+            echo "Stop postion is: $OPTARG";
+            STOP=$OPTARG
+            I=2
+            ;;
+        f)
+            echo "Using: $OPTARG samples";
+            TYPE=$OPTARG
+            ;;
+        o)
+            echo "The file will be output to: ${OPTARG%/}.vcf"
+            OUTPUT="${OPTARG%/}"
+            ;;
+        ?)
+            echo "not a valid argument, try using the -h option for usage information"
+            exit 2
+            ;;
+    esac
+done
+
+#just madke a list of available chromosomes
+CHR="01 02 03 04 05 06 07 08 09 10 11 12 13 14 15 16 17 18 19 20"
+
+#see if the option is not in the list, if so kill script
+if echo $CHR | grep -vw $CHROM > /dev/null; then
+    echo "error: you must use a number between 01-20 for chromosome (-t)" >&2; exit 1
+fi
+
+#make sure that the start and stop options are numbers
+NUM='^[0-9]+$'
+if ! [[ $START =~ $NUM ]] ; then
+   echo "error: you must use a number for start postion (-s)" >&2; exit 1
+fi
+
+NUM='^[0-9]+$'
+if ! [[ $STOP =~ $NUM ]] ; then
+   echo "error: you must use a number for stop postion (-t)" >&2; exit 1
+fi
+
+if [ $START >= $STOP ] ; then
+   echo "error: your start position is larger than your stop" >&2; exit 1
+fi
+
+if [ $TYPE == "NAM" ]
+    then
+    FILE="/mnt/d/NAMVCF/NAM_Parent_Playpus.vcf"
+    HEADER="/mnt/d/NAMVCF/PlatHeaders.vcf"
+fi
+
+#grab the header from a pre-parsed file to save time
+$(grep "#" $HEADER > $OUTPUT.vcf)
+#give a warning so they don't think it hangs
+echo "Searching through VCF file, this can take up to five minutes"
+
+#grep by chromosome since it is faster then filter based on pos using awk
+$(grep -e "Chr$CHROM"  $FILE | awk -v start="$START" -v stop="$STOP" '$2 >= start && $2 <= stop' >> $OUTPUT.vcf)
